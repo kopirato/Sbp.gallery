0c57edad6d4e15c418dc2c6b2971d54f1f23bbd2
Selian.T <84496338+kopirato@users.noreply.github.com>
2026-06-28 20:44:03 +0300

    Implement image upload and gallery display
    Add functionality to upload images and display them in the gallery.

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-const photos=document.querySelectorAll(".gallery img");
+const upload =
+document.getElementById("upload");
 
 
-photos.forEach(photo=>{
+const gallery =
+document.getElementById("gallery");
 
 
-photo.onclick=()=>{
+
+upload.addEventListener("change",function(){
 
 
-photo.requestFullscreen();
+const files=this.files;
+
+
+
+for(let file of files){
+
+
+const reader=new FileReader();
+
+
+
+reader.onload=function(e){
+
+
+
+let card=document.createElement("div");
+
+card.className="card";
+
+
+
+card.innerHTML=`
+
+<img src="${e.target.result}">
+
+
+<div class="info">
+
+Uploaded:
+
+<br>
+
+${new Date().toLocaleString()}
+
+</div>
+
+`;
+
+
+
+gallery.prepend(card);
+
 
 
 }
+
+
+
+reader.readAsDataURL(file);
+
+
+}
+
 
 
 });
